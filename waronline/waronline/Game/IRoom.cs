7dbb2f71d8d9390227130a28305993b2f0f443c7
rvarna <rakeshvarna@gmail.com>
2014-01-09 22:59:30 -0800

    Added the transport and related interfaces
    Added IRoom, IMessage and ICloud interfaces.
    Created corresponding tables in Azure.

---
+++ waronline/waronline/Game/IRoom.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace waronline.Game
+{
+    public interface IRoom
+    {
+        /// <summary>
+        /// Gets or sets the room id.
+        /// </summary>
+        string RoomId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the room.
+        /// </summary>
+        string RoomName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the username of the creator of the room.
+        /// </summary>
+        string CreatedBy { get; set; }
+
+        /// <summary>
+        /// Gets or sets the list of users present in the room.
+        /// </summary>
+        IList<string> UsersInRoom { get; set; }
+    }
+}
---
+++ waronline/waronline/Transport/ICloud.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using waronline.Game;
+
+namespace waronline.Transport
+{
+    public interface ICloud
+    {
+        /// <summary>
+        /// Creates a user in the cloud.
+        /// </summary>
+        /// <param name="username">The username of the user being created.</param>
+        void CreateUser(string username);
+
+        /// <summary>
+        /// Creates a room in the cloud.
+        /// </summary>
+        /// <param name="roomName">The name of the room being created.</param>
+        /// <returns>The created <see cref="IRoom"/>.</returns>
+        IRoom CreateRoom(string roomName);
+
+        /// <summary>
+        /// Join an existing room.
+        /// </summary>
+        /// <param name="roomName">The name of the room to join.</param>
+        /// <param name="username">The username of the user joining the specified room.</param>
+        void JoinRoom(string roomName, string username);
+
+        /// <summary>
+        /// View the list of existing rooms.
+        /// </summary>
+        /// <param name="friendNames">
+        /// A list of names of the friends of the current user.
+        /// If not null, only the rooms created by these users are returned.
+        /// </param>
+        /// <returns></returns>
+        IList<IRoom> ViewRooms(IList<string> friendNames);
+
+        /// <summary>
+        /// Sends a message to other users in the room.
+        /// </summary>
+        /// <param name="message">The <see cref="IMessage"/> being sent.</param>
+        void SendMessage(IMessage message);
+    }
+}
