94b381f5c2af18e3306c3c5f424fa04079d6f756
Phillip LeBlanc <phillip.leblanc@outlook.com>
2014-01-20 20:51:27 -0800

    Added the GameLobby page

---
+++ waronline/waronline/GameLobby.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+using waronline.Data;
+
+namespace waronline
+{
+    public partial class GameLobby : PhoneApplicationPage
+    {
+        public static IRoom CurrentRoom { get; set; }
+
+        public GameLobby()
+        {
+            InitializeComponent();
+
+            foreach (string user in CurrentRoom.UsersInRoom)
+            {
+                var textBlock = new TextBlock { Text = user };
+                PlayerList.Children.Add(textBlock);
+            }
+
+            this.Unloaded += GameLobby_Unloaded;
+        }
+
+        void GameLobby_Unloaded(object sender, RoutedEventArgs e)
+        {
+            CurrentRoom = null;
+        }
+    }
+}
