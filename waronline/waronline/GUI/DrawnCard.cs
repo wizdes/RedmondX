1b1c441fd9ce004325a6c38b34ba9ef7c012b51a
Phillip LeBlanc <pmleblanc007@gmail.com>
2014-01-26 15:17:14 -0800

    Initial port of GUI over to the waronline project

---
+++ waronline/waronline/GUI/DrawnCard.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Phone.Maps.Controls;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace waronline
+{
+    class DrawnCard
+    {
+        private int x;
+        private int y;
+        private int x_size = 75;
+        private int y_size = 110;
+        private Texture2D reference;
+        private bool isVisible;
+        private bool isMoving;
+
+        public int X
+        {
+            get { return x; }
+        }
+
+        public int Y
+        {
+            get { return y; }
+        }
+
+        public Texture2D Reference
+        {
+            get { return reference; }
+        }
+
+        public bool containsPosition(Vector2 position){
+            if (position.X > x && position.X < x + x_size &&
+                position.Y > y && position.Y < y + y_size)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public DrawnCard(Texture2D reference)
+        {
+            this.reference = reference;
+            isVisible = false;
+            isMoving = false;
+            x = 0;
+            y = 0;
+        }
+
+        public bool IsVisible
+        {
+            get
+            {
+                return isVisible;
+            }
+            set { isVisible = value; }
+        }
+
+        public bool IsMoving
+        {
+            get
+            {
+                return isMoving;
+            }
+            set { isMoving = value; }
+        }
+
+        public void move(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
---
+++ waronline/waronline/GUI/Game1.cs
@@ -0,0 +1,188 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Phone.Maps.Controls;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input.Touch;
+
+namespace waronline
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager _graphics;
+        SpriteBatch _spriteBatch;
+        private List<DrawnCard> cardTextureList;
+        private Dictionary<string, DrawnCard> cardTextureMap;
+        private int x = 0;
+        private int y = 0;
+        private bool _firstUpdate = true;
+
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = @"Assets\Cards";
+            cardTextureList = new List<DrawnCard>();
+            cardTextureMap = new Dictionary<string, DrawnCard>();
+
+            TouchPanel.EnabledGestures = GestureType.Tap;
+        }
+
+
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            string[] cardType =
+            {
+                "Club",
+                "Diamond",
+                "Heart",
+                "Spade"
+            };
+
+            string[] specialCardValues =
+            {
+                "jack",
+                "queen",
+                "king",
+                "ace"
+            };
+
+            foreach(string cardPrefix in cardType)
+            {
+                for (int i = 2; i < 11; i++)
+                {
+                    string cardName = cardPrefix + "_" + i.ToString();
+                    DrawnCard reference = new DrawnCard(Content.Load<Texture2D>(cardName));
+                    cardTextureList.Add(reference);
+                    cardTextureMap.Add(cardName, reference);
+                }
+
+                foreach (string specialCardValue in specialCardValues)
+                {
+                    string cardName = cardPrefix + "_" + specialCardValue;
+                    DrawnCard reference = new DrawnCard(Content.Load<Texture2D>(cardName));
+                    cardTextureList.Add(reference);
+                    cardTextureMap.Add(cardName, reference);
+                }
+            }
+
+
+            Random r = new Random();
+            foreach (DrawnCard card in cardTextureList)
+            {
+                card.IsVisible = true;
+                card.move(r.Next(480), r.Next(800));
+            }
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        protected void removeCardAtPosition(Vector2 position)
+        {
+            DrawnCard cardToRemove = null;
+            foreach (DrawnCard card in cardTextureList)
+            {
+                if (card.containsPosition(position))
+                {
+                    cardToRemove = card;
+                }
+            }
+            if (cardToRemove != null)
+            {
+                cardToRemove.IsVisible = false;
+                cardToRemove.move(-100, -100);
+            }
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (_firstUpdate)
+            {
+                // Temp hack to fix gestures
+                typeof(Microsoft.Xna.Framework.Input.Touch.TouchPanel)
+                    .GetField("_touchScale", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static)
+                    .SetValue(null, Vector2.One);
+
+                _firstUpdate = false;
+            }
+
+            while (TouchPanel.IsGestureAvailable)
+            {
+                GestureSample gesture = TouchPanel.ReadGesture();
+
+                switch (gesture.GestureType)
+                {
+                    case GestureType.Tap:
+                        removeCardAtPosition(gesture.Position);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.ForestGreen);
+
+            Vector2 position = new Vector2(x, y);
+
+            _spriteBatch.Begin();
+
+            //_spriteBatch.Draw(_spriteTexture, position, Color.White);
+
+            foreach (DrawnCard card in cardTextureList)
+            {
+                _spriteBatch.Draw(card.Reference, new Vector2(card.X, card.Y), Color.White);
+            }
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
